762565b43fd1dc1597731750f57655a0ae454efd
beer <surakiat.kongchana@enov8.com>
2024-07-01 12:12:10 +0700

    import docs, plotly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Import packages
-from dash import Dash, html, dash_table
+from dash import Dash, html, dash_table, docs
 import pandas as pd
+import plotly.express as px
 
 # Incorporate data
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
@@ -11,7 +12,7 @@ app = Dash()
 # App layout
 app.layout = [
     html.Div(children='My First App with Data'),
-    dash_table.DataTable(data=df.to_dict('records'), page_size=20) #default page size is 10 
+    dash_table.DataTable(data=df.to_dict('records'), page_size=15) #default page size is 10 
 ]
 
 # Run the app
