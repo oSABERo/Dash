763b6e9293e42da308189dce6a9b531cb6f406d6
beer <surakiat.kongchana@enov8.com>
2024-07-01 12:07:09 +0700

    import pandas

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # Import packages
-from dash import Dash, html
+from dash import Dash, html, dash_table
+import pandas as pd
 
 # Initialize the app
 app = Dash()
 
 # App layout
-app.layout = [html.Div(children='Hello World')]
+app.layout = [html.Div(children='My Dash Table')]
 
 # Run the app
 if __name__ == '__main__':
